c9c30da4504fb72fcf502a6e0c9c0259c9e93a7b
karanOO89 <gk.987680@gmail.com>
2021-04-29 04:15:39 +0000

    1_queries completed

---
+++ 1_queries/4_most_visited_cities.sql
@@ -0,0 +1,5 @@
+SELECT city as city ,count(reservations.*) as total_reservations
+FROM properties 
+JOIN reservations ON reservations.property_id = properties.id 
+GROUP BY city
+ORDER BY total_reservations DESC;
